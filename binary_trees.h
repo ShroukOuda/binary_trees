b30be7276e3bd995f8a6623c0a0977a9a2e8e575
ShroukOuda <s36568961@gmail.com>
2024-06-06 22:29:22 +0300

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,18 +20,12 @@ typedef struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 } binary_tree_t;
-
 typedef struct binary_tree_s binary_tree_t; /** Binary Tree */
-
 typedef struct binary_tree_s bst_t; /** Binary Search Tree */
-
 typedef struct binary_tree_s avl_t; /** AVL Tree */
-
 typedef struct binary_tree_s heap_t; /** Max Binary Heap */
-
 /** Print_Function */
 void binary_tree_print(const binary_tree_t *);
-
 /** Binary_Tree */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -51,4 +45,4 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
