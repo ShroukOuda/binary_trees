c91c0d32fe9fe46ead5c4b69ba7500fe81a57290
ShroukOuda <s36568961@gmail.com>
2024-07-09 17:12:31 +0300

    commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,10 +20,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_height;
+
 	if (!tree)
 		return (0);
-	int left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-	int right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
-
+	left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
+	right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
 	return (left_height - right_height);
 }
